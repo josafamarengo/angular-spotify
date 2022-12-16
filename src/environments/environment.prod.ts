c428fe93ead86a1311c388af0d4522ddb0485681
josafamarengo <josafabmarengo@gmail.com>
2022-12-16 09:12:26 -0400

    config: add netlify.toml

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,21 @@
 export const environment = {
   production: true,
 }
+
+export const SpotifyConfig = {
+  clientId: '36af13d996d64f54bb674161f729f4bc',
+  redirectUrl: 'https://radinho.netlify.app/login/',
+  authEndPoint: 'https://accounts.spotify.com/authorize',
+  scopes: [
+    "user-read-currently-playing",
+    "user-read-playback-state",
+    "user-modify-playback-state",
+    "user-read-recently-played",
+    "user-top-read",
+    "user-read-playback-position",
+    "user-library-read",
+    "user-library-modify",
+    "playlist-read-private",
+    "playlist-read-collaborative",
+  ]
+}
