c60c493fdd64095496faf388e8412560fdee57da
josafamarengo <josafabmarengo@gmail.com>
2022-12-15 20:12:03 -0400

    feat: add factories

---
+++ src/app/shared/factories.ts
@@ -0,0 +1,46 @@
+import {ITrack} from "../interfaces/ITrack";
+
+export function newArtist() {
+  return {
+    id: '',
+    name: '',
+    genres: [],
+    images: [],
+    topTracks: [],
+  };
+}
+
+export function newTrack() {
+  return {
+    id: '',
+    name: '',
+    album: newAlbum(),
+    artists: [
+      newArtist(),
+    ],
+    duration_ms: "",
+    explicit: false,
+    preview_url: '',
+    uri: '',
+  };
+}
+
+export function newAlbum() {
+  return {
+    id: '',
+    name: '',
+    image: '',
+    tracks: [],
+    type: '',
+    uri: '',
+  };
+}
+
+export function newPlaylist() {
+  return {
+    id: '',
+    name: '',
+    image: '',
+    tracks: [],
+  };
+}
